b0dc9c937d3f34020ed1a1548a0098018fe20c94
HSLoveZL <hjmrezl@outlook.com>
2016-11-03 15:11:15 +0800

    add demo04 directory

---
+++ demo04/demos01.py
@@ -0,0 +1,11 @@
+def calc_md5(password):
+    pass
+
+
+def login(user, password):
+    pass
+
+
+db = {}
+def register(username, password):
+    db{username} = get_md5(password + username + 'the-Salt')
