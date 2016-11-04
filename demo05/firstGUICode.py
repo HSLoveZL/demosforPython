11f64304f44ab01f604406b2b4a33cd932c03702
HSLoveZL <hjmrezl@outlook.com>
2016-11-04 17:13:26 +0800

    modify  file demo05.py

--- demo05/firstGUICode.py
+++ demo05/firstGUICode.py
@@ -1,18 +1,25 @@
 from Tkinter import *
+import tkMessageBox
 
 
 class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.quitButton = Button(self, text='quit', command=self.quit)
-        self.helloLabel = Label(self, text='Hello,World!')
+        # self.quitButton = Button(self, text='Hello', command=self.hello)
+        # self.helloLabel = Label(self, text='Hello,World!')
+        self.nameInput = Entry(self)
+        self.alertButton = Button(self, text='Hello', command=self.hello)
         self.pack()
-        self.createWidgets()
+        self.createwidgets()
 
-    def createWidgets(self):
-        self.helloLabel.pack()
-        self.quitButton.pack()
+    def createwidgets(self):
+        self.nameInput.pack()
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get()
+        tkMessageBox.showinfo('Message', 'Hello, %s' % name)
 
 app = Application()
-app.master.title('Hello,World!')
+# app.master.title('Hello,World!')
 app.mainloop()
